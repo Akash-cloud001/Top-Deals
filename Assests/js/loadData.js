0f11786f0ccd5e729a879d4c413921cbb7375d5e
Akash Parmar <ak.parmar1442000@gmail.com>
2023-01-27 19:42:28 +0530

    DebuggedCategoriesButton

--- Assests/js/loadData.js
+++ Assests/js/loadData.js
@@ -1,82 +1,80 @@
-const productSection = $("#amazonDeals");
+$(document).ready(()=>{
+    const productSection = $("#amazonDeals");
 
-function handleInfinteScroll(){
-    // console.log(window.innerHeight + window.pageYOffset, document.body.getBoundingClientRect().height)
-    if(window.innerHeight + window.pageYOffset + 300 >= document.body.getBoundingClientRect().height){
-        //ajax call the server for the cards 
-        //after getting the response as a set of cards in json format 
-        //then we convert the json file into the object format and
-        // we will make the specific card container from another function via passing objects
-        //if the the response was a failure menas we had shown all the data then we call a function which will hide the loader from the screen depicting that we are out of cards
-        ajaxRequest();
-        console.log('HI');
-    }
-
-}
-
-function createTheProductCards(productData){
-    console.log(productData);
-    const products = productData.data;
-    for(let product in products){
-        productSection.append(`
-        <div class="product-card-container">
-            <figure class="product-card-img">
-                <img src=${products[product].avatar} alt="" class="product-img">
-            </figure>
-            <div class="product-details">
-                <h2 class="product-header">
-                    ${products[product].first_name}
-                </h2>
-                <h3 class="product-price">
-                    ${products[product].id}&nbsp;&#x20B9;
-                </h3>
-                <p class="product-defination">
-                    ${products[product].last_name}                    
-                </p>
-                <a href=${products[product].email} class="product-anchor" target="_blank ">Read more</a>
-            </div>
-        </div>
-    `);
-    }
-}
-
-/*
-    object structure
-    {
-        total:10,
-        data:[
-            {...}
-            {...}
-            {...}
-            .
-            .
-            .
-            {...}
-        ]
-    }
-
-*/
-
-function ajaxRequest(){
-    $.ajax({
-        url:"https://reqres.in/api/users?page=1",
-        type : 'GET',
-        success : function(data){
-            console.log(data);
-            const productData = {
-                // size = responseTxt.total;  we gonna use total but at this instance we use perpage bcuz of demo url
-                size : data.per_page,
-                data : data.data,
-            }
-            createTheProductCards(productData);
-        },
-        error: ()=>{
-            alert('error data not found')
+    function handleInfinteScroll(){
+        if(window.innerHeight + window.pageYOffset + 300 >= document.body.getBoundingClientRect().height){
+            //ajax call the server for the cards 
+            //after getting the response as a set of cards in json format 
+            //then we convert the json file into the object format and
+            // we will make the specific card container from another function via passing objects
+            //if the the response was a failure menas we had shown all the data then we call a function which will hide the loader from the screen depicting that we are out of cards
+            ajaxRequest();
         }
-    })
-}
-
-window.onload = ()=>{
-    ajaxRequest();
-}
-window.addEventListener('scroll', handleInfinteScroll);
+    
+    }
+    
+    function createTheProductCards(productData){
+        const products = productData.data;
+        for(let product in products){
+            productSection.append(`
+            <div class="product-card-container">
+                <figure class="product-card-img">
+                    <img src=${products[product].avatar} alt="" class="product-img">
+                </figure>
+                <div class="product-details">
+                    <h2 class="product-header">
+                        ${products[product].first_name}
+                    </h2>
+                    <h3 class="product-price">
+                        ${products[product].id}&nbsp;&#x20B9;
+                    </h3>
+                    <p class="product-defination">
+                        ${products[product].last_name}                    
+                    </p>
+                    <a href=${products[product].email} class="product-anchor" target="_blank ">Read more</a>
+                </div>
+            </div>
+        `);
+        }
+    }
+    
+    /*
+        object structure
+        {
+            total:10,
+            data:[
+                {...}
+                {...}
+                {...}
+                .
+                .
+                .
+                {...}
+            ]
+        }
+    
+    */
+    
+    function ajaxRequest(){
+        $.ajax({
+            url:"https://reqres.in/api/users?page=1",
+            type : 'GET',
+            success : function(data){
+                const productData = {
+                    // size = responseTxt.total;  we gonna use total but at this instance we use perpage bcuz of demo url
+                    size : data.per_page,
+                    data : data.data,
+                }
+                createTheProductCards(productData);
+            },
+            error: ()=>{
+                alert('error data not found')
+            }
+        })
+    }
+    
+    window.onload = ()=>{
+        ajaxRequest();
+    }
+    window.addEventListener('scroll', handleInfinteScroll);
+})
--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,6 +1,6 @@
 const dropdown = document.getElementById("dropdown");
 const dropdownLayer = document.getElementById("dropdown-lay1");
-const items = document.querySelectorAll(".nav-dropdown-anchor");
+// const items = document.querySelectorAll(".nav-dropdown-anchor");
 const navUnorderedList = document.getElementById("nav-ul-id");
 
 const lineOne = document.getElementById("line-first");
@@ -8,13 +8,9 @@ const lineTwo = document.getElementById("line-second");
 const lineThree = document.getElementById("line-third");
 
 
-function toggleClass(){
+function toggleClass(event){
     dropdown.classList.toggle("hide");
     dropdownLayer.classList.toggle("hide");
-    if(dropdownLayer.classList.contains("hide") && navUnorderedList.classList.contains("nav-ul-slider")){
-        toggleClass();
-        hamburgerFunct();
-    }
 }
 
 function removeToggleClass(){
@@ -24,12 +20,8 @@ function removeToggleClass(){
     }
 }
 
-items.forEach(ele=>ele.addEventListener('click', ()=>{
-    toggleClass();
-}));
-
-function hamburgerFunct(){
-    // dropdownLayer2.classList.toggle("hide");
+function hamburgerFunct(event){
+    dropdownLayer.classList.toggle("hide");
     lineOne.classList.toggle("line-f");
     lineTwo.classList.toggle("line-s");
     lineThree.classList.toggle("line-t");
